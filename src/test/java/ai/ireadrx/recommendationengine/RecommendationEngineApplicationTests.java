8340346b188bc24465ed574a874533d7d6a6df84
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-07-08 00:16:59 +0530

    :tada: Initialize project

---
+++ src/test/java/ai/ireadrx/recommendationengine/RecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.ireadrx.recommendationengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
