febeaf6656bfeb32a58c947b3fdbb1144a25ff97
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-07-21 21:50:13 +0530

    :sparkles: Add controller for patents

--- src/main/java/ai/ireadrx/recommendationengine/service/SearchService.java
+++ src/main/java/ai/ireadrx/recommendationengine/service/SearchService.java
@@ -6,15 +6,19 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import ai.ireadrx.recommendationengine.entity.ChemicalEntity;
+import ai.ireadrx.recommendationengine.entity.PatentEntity;
 import ai.ireadrx.recommendationengine.repository.ChemicalRepository;
+import ai.ireadrx.recommendationengine.repository.PatentRepository;
 
 @Service
 public class SearchService {
 
 	private final ChemicalRepository chemicalRepository;
+	private final PatentRepository patentRepository;
 
-	public SearchService(ChemicalRepository chemicalRepository) {
+	public SearchService(ChemicalRepository chemicalRepository, PatentRepository patentRepository) {
 		this.chemicalRepository = chemicalRepository;
+		this.patentRepository = patentRepository;
 	}
 
 	/**
@@ -30,13 +34,20 @@ public class SearchService {
 	/**
 	 * Get a list of all chemicals
 	 * 
-	 * @return A list of all the chemical nodes
+	 * @param resultLimit Optional. Specify a number to return that many values
+	 * @return
 	 */
-	public List<ChemicalEntity> getAllChemicals() {
-		return chemicalRepository.findAll();
-	}
-
 	public List<ChemicalEntity> getAllChemicalsUsingCustomQuery(int resultLimit) {
 		return chemicalRepository.findByCustomQuery(resultLimit);
+	}
+
+	/**
+	 * Get a list of all patents
+	 * 
+	 * @param resultLimit Optional. Specify a number to return that many values
+	 * @return
+	 */
+	public List<PatentEntity> getAllPatentsUsingCustomQuery(int resultLimit) {
+		return patentRepository.findAllByCustomQuery(resultLimit);
 	}
 }
